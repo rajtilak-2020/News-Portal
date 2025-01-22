da4640dc47a27c824aa903c37e406b1523c4d7d6
K Rajtilak <rajtilak1062020@gmail.com>
2025-01-22 09:40:03 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    //API_KEY: '44d87147b480e83c7d5eac3745ab6e8f',
+    API_KEY: '44d87147b480e83c7d5eac3745ab6e8f',
     API_BASE_URL: 'https://gnews.io/api/v4',
     ARTICLES_PER_PAGE: 12,
     DEBOUNCE_DELAY: 300,
